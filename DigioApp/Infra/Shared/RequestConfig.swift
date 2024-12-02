397d1d65b07cff73e088f2c1f9480372245d34e2
Rubens <rubens.machion@gmail.com>
2024-12-02 18:43:18 -0300

    no message

--- DigioApp/Infra/Shared/RequestConfig.swift
+++ DigioApp/Infra/Shared/RequestConfig.swift
@@ -2,7 +2,6 @@ import Foundation
 import AppProtocols
 
 struct RequestConfig: RequestProtocol {
-    // https://7hgi9vtkdc.execute-api.sa-east-1.amazonaws.com/sandbox/products
     var sheme: String = "https"
     var host: String = "7hgi9vtkdc.execute-api.sa-east-1.amazonaws.com"
     var port: Int?
